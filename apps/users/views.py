e4c75908417292844d2e104b8a753b0f02c1641b
Matveev Evgeniy <jeka150109@mail.ru>
2026-07-09 16:41:32 +0300

    - добавлены права доступа

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,16 +1,17 @@
 from rest_framework.generics import (ListAPIView, CreateAPIView, GenericAPIView, 
                                     UpdateAPIView, DestroyAPIView, RetrieveAPIView)
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from users.models import User
-from users.user_serializer import (UserSerializer, UserCreateSerializer,
- UserUpdateSerializer, UserPasswordSerializer, UserTokenObtainSerializer)
+from apps.users.models import User
+from apps.users.permissions import IsAdmin, IsModerator, IsOwner
+from apps.users.user_serializer import (UserSerializer, UserCreateSerializer,
+ UserUpdateSerializer, UserPasswordSerializer)
 
 class UserListAPIView(ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsModerator, )
 
 class UserCreateAPIView(CreateAPIView):
     queryset = User.objects.all()
@@ -20,12 +21,12 @@ class UserCreateAPIView(CreateAPIView):
 class UserRetrieveAPIView(RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (AllowAny, )
+    permission_classes = (IsOwner | IsModerator, )
 
 class UserUpdateAPIView(UpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserUpdateSerializer
-    permission_classes = (AllowAny, )
+    permission_classes = (IsOwner | IsModerator, )
 
     def get_object(self):
         user = self.request.user
@@ -33,23 +34,26 @@ class UserUpdateAPIView(UpdateAPIView):
 
 class UserPasswordUpdateAPIView(GenericAPIView):
     serializer_class = UserPasswordSerializer
-    permission_classes = (AllowAny, )
+    permission_classes = (IsOwner | IsAdmin, )
 
     def post(self, request):
-        serializer = self.get_serializer(data=request)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception = True)
 
+        old = serializer.validated_data['old_password']
+        new = serializer.validated_data['new_password']
+        
+        if old == new:
+            return Response({'error': 'Новый пароль должен отличаться от старого'}, status=400)
+
         user = request.user
-        if not user.check_password(serializer.validated_data['old_password']):
+        if not user.check_password(old):
             return Response({'error': 'Неверный старый пороль'}, status=400)
         
-        user.set_password(serializer.validated_data['new_password'])
+        user.set_password(new)
         user.save()
         return Response({'success': True})
 
 class UserDeleteAPIView(DestroyAPIView):
     queryset = User.objects.all()
-    permission_classes = (AllowAny, )
-
-class UserTokenObtainPairView(TokenObtainPairView):
-    pass
+    permission_classes = (IsOwner | IsAdmin, )

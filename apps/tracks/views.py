1c23934417e59871f00f27e977266d8d03c6b586
Matveev Evgeniy <jeka150109@mail.ru>
2026-07-11 08:06:10 +0300

    - Добавлены views треков

--- apps/tracks/views.py
+++ apps/tracks/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.generics import (ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView)
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from apps.tracks.models import Track, Genre
+from apps.users.permissions import IsArtist, IsModerator, IsAdmin, IsOwner
+from apps.tracks.track_serializer import TrackSerializer, TrackCreateSerializer, TrackUpdateSerializer
+
+class TrackListAPIView(ListAPIView):
+    queryset = Track.objects.all()
+    serializer_class = TrackSerializer
+    permission_classes = (AllowAny, )
+
+class TrackDetailAPIView(RetrieveAPIView):
+    queryset = Track.objects.all()
+    serializer_class = TrackSerializer
+    permission_classes = (AllowAny, )
+
+class TrackCreateAPIView(CreateAPIView):
+    queryset = Track.objects.all()
+    serializer_class = TrackCreateSerializer
+    permission_classes = (IsArtist, )
+
+class TrackUpdateAPIView(UpdateAPIView):
+    queryset = Track.objects.all()
+    serializer_class = TrackUpdateSerializer
+    permission_classes = (AllowAny, )
+
+class TrackDeleteAPIView(DestroyAPIView):
+    queryset = Track.objects.all()
+    permission_classes = (AllowAny, )

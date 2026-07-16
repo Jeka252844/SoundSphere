90c55d00ff814de25e7655517a58525448b9e88b
Matveev Evgeniy <jeka150109@mail.ru>
2026-07-16 09:58:37 +0300

    - Добавлены urls для альбомов

--- apps/artists/urls.py
+++ apps/artists/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
 from apps.artists.views import (ArtistListAPIView, ArtistDetailAPIView, ArtistCreateAPIView,
-ArtistDeleteAPIView, ArtistUpdateAPIView, SearchAristsView, GetTopArtistsView)
+ArtistDeleteAPIView, ArtistUpdateAPIView, SearchAristsView, GetTopArtistsView,
+AlbumCreateAPIView, AlbumDetailAPIView, AlbumDeleteAPIView, AlbumUpdateAPIView)
+
 
 app_name = 'artists'
 
 urlpatterns = [
+    # artist
     path('', ArtistListAPIView.as_view(), name='artist_list'),
     path('<int:pk>/', ArtistDetailAPIView.as_view(), name='artist_detail'),
     path('create/', ArtistCreateAPIView.as_view(), name='artist_create'),
@@ -13,4 +16,10 @@ urlpatterns = [
     path('<int:pk>/delete/', ArtistDeleteAPIView.as_view(), name='artist_delete'),
     path('top/', GetTopArtistsView.as_view(), name='top_artists'),
     path('search/', SearchAristsView.as_view(), name='search_artists'),
+
+    # album
+    path('album/<int:pk>/', AlbumDetailAPIView.as_view(), name="album_detail"),
+    path('album/create/', AlbumCreateAPIView.as_view(), name='album_create'),
+    path('album/<int:pk>/update/', AlbumUpdateAPIView.as_view(), name='album_update'),
+    path('album/<int:pk>/delete/', AlbumDeleteAPIView.as_view(), name='album_delete'),
 ]

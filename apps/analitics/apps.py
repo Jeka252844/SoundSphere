8ceed3738b2e4cdfd3d4d4a9380774ca92afb307
Matveev Evgeniy <jeka150109@mail.ru>
2026-07-27 08:19:19 +0300

    -создана модель истории прослушивания - Добавленн сервис для получения топа треков по прослушиваниям за неделю

--- apps/analitics/apps.py
+++ apps/analitics/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class AnaliticsConfig(AppConfig):
-    name = 'analitics'
+    name = 'apps.analitics'
--- apps/analitics/models.py
+++ apps/analitics/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class ListeningHistory(models.Model):
+    user = models.ForeignKey('apps.users.User', on_delete=models.CASCADE, related_name='listening_history', verbose_name='пользователь')
+    track = models.ForeignKey('apps.tracks.Track', on_delete=models.CASCADE, related_name='listening_history',verbose_name='трек')
+    listened_at = models.DateTimeField(auto_now_add=True, verbose_name='дата прослушивания')
+
+    class Meta:
+        ordering = ['-listened_at']
+        verbose_name = 'История прослушиваний'
+        verbose_name_plural = 'История прослушиваний'
+
+    def __str__(self):
+        return f'{self.track.title} — {self.listened_at}'
+    
+
+class DailyStats(models.Model):
+    track = models.ForeignKey('tracks.Track', on_delete=models.CASCADE, related_name='daily_stats')
+    date = models.DateField()
+    plays = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    
+    class Meta:
+        unique_together = ['track', 'date']

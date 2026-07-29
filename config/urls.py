6ca26250438c4841479b02d2ba293dc28257840f
Matveev Evgeniy <jeka150109@mail.ru>
2026-07-29 10:02:08 +0300

    - Созданы стили и шаблон профиля исполнителя - Добавлен url на профиль исполнителя в базовый шаблон

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('player/', views.player, name='player'),
     path('artists/', views.artists, name='artists'),
+    path('artist/my/', views.artist_profile_my_view, name='artist_profile_my'),
+    path('artist/<int:artist_id/', views.artist_profile_view, name='artist_profile'),
     path('album/', views.album, name='album'),
 
     # base_url
